558fb1135a481561da135e26b44f0e978a4ac43f
ahmedzafar7 <ahmed.zafar@7vals.com>
2016-04-16 12:04:04 -0700

    just for fun

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ActiveRecord::Base
-  validates :price, numericality: true
+  validates :name, :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
